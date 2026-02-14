445edc64c651d65c6e9e0057a8f7ecfc52d93f0a
Vitaly <vitaly202@gmail.com>
2026-02-14 04:13:17 -0500

    initial coords on home page are now based on url params. Added both GeolocateControl to map, and current location button functionality.

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,22 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { Ref, useEffect, useRef, useState } from "react";
 import { GeoCoords } from "./types";
-import Map, { Marker, useMap } from "react-map-gl/maplibre";
+import Map, { GeolocateControl, Marker, useMap } from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
 import { createMockCoords } from "./mock-coords";
+import { useParams } from "next/navigation";
 
 const NYC_COORDS: GeoCoords = { longitude: -73.935242, latitude: 40.73061 };
 
 export const HomePage = () => {
-  const [currentCoords, setCurrentCoords] = useState<GeoCoords>();
+  const { longitude: param_longitude, latitude: param_latitude } = useParams();
+  const [initCoords, setInitCoords] = useState<GeoCoords>({
+    longitude: param_longitude ? Number(param_longitude) : NYC_COORDS.longitude,
+    latitude: param_latitude ? Number(param_latitude) : NYC_COORDS.latitude,
+  });
 
-  useEffect(() => {
-    if ("geolocation" in navigator) {
-      // Retrieve latitude & longitude coordinates from `navigator.geolocation` Web API
-      navigator.geolocation.getCurrentPosition((pos) => {
-        const { latitude, longitude } = pos.coords;
-        // Fetch data from API with given latitude and longitude
-        setCurrentCoords({ latitude, longitude });
-      });
-    }
-  }, []);
+  const goHomeRef = useRef<HTMLDivElement>(null);
 
   return (
     <div className="h-screen w-screen flex flex-col bg-slate-700 overflow-hidden">
@@ -29,57 +26,65 @@ export const HomePage = () => {
       <div className="flex-1 flex justify-center items-center h-[500px] w-full overflow-hidden">
         <Map
           initialViewState={{
-            longitude: NYC_COORDS.longitude,
-            latitude: NYC_COORDS.latitude,
+            longitude: initCoords.longitude,
+            latitude: initCoords.latitude,
             zoom: 16,
           }}
           minZoom={10}
           maxZoom={20}
           mapStyle="https://api.maptiler.com/maps/streets/style.json?key=wj2xyR5KXFoBWjTtFjY7"
         >
-          {<FindingLocation loc={currentCoords} />}
-          {currentCoords &&
-            createMockCoords(currentCoords, 0.002).map((e, i) => (
-              <Marker
-                key={`mockLoc${i}`}
-                longitude={e.longitude}
-                latitude={e.latitude}
-                anchor="center"
-              >
-                <div className="h-4 w-4 rounded-full border-4 border-blue-800" />
-              </Marker>
-            ))}
+          {createMockCoords(initCoords, 0.002).map((e, i) => (
+            <Marker
+              key={`mockLoc${i}`}
+              longitude={e.longitude}
+              latitude={e.latitude}
+              anchor="center"
+            >
+              <div className="h-7 w-7 rounded-full border-6 border-blue-800" />
+            </Marker>
+          ))}
+          <GeolocateControl position="bottom-left" />
+          <GoHome ref={goHomeRef} />
         </Map>
       </div>
       <div className="p-4 flex justify-center">
         <button
           className="text-md py-2 px-4 rounded-full bg-slate-500 cursor-pointer"
-          onClick={() => {}}
+          onClick={() => {
+            goHomeRef.current?.click();
+          }}
         >
-          Go to my location
+          {`Go to my location`}
         </button>
       </div>
     </div>
   );
 };
 
-const FindingLocation = ({ loc }: { loc?: GeoCoords }) => {
+const GoHome = ({ ref }: { ref: Ref<HTMLDivElement> }) => {
   const { current: map } = useMap();
+  const [currentCoords, setCurrentCoords] = useState<GeoCoords>();
 
   useEffect(() => {
-    if (map && loc) {
-      map.flyTo({
-        center: [loc.longitude, loc.latitude],
-        essential: true, // this animation is considered essential with respect to prefers-reduced-motion
-      });
-    }
-  }, [map, loc]);
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const { latitude, longitude } = pos.coords;
+      setCurrentCoords({ latitude, longitude });
+    });
+  }, []);
 
-  return loc ? (
-    <></>
-  ) : (
-    <div className="absolute left-1/2 top-1/2 -translate-1/2 rounded-full p-4 text-2xl bg-slate-900 text-slate-50 animate-pulse">
-      Getting Your Location
-    </div>
+  return (
+    <div
+      ref={ref}
+      className="pointer-events-none"
+      onClick={() => {
+        if (map && currentCoords) {
+          map.flyTo({
+            center: [currentCoords.longitude, currentCoords.latitude],
+            essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+          });
+        }
+      }}
+    />
   );
 };
