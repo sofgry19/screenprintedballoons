01a05125204c8292a89b147d5bc94f66bdc47213
Vitaly <vitaly202@gmail.com>
2026-02-14 02:35:54 -0500

    map now loads with fixed coords, then flies to current location once found.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Map, { Marker } from "react-map-gl/maplibre";
+import Map, { Marker, useMap } from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { useEffect, useState } from "react";
 import { createMockCoords } from "./mock-coords";
@@ -11,7 +11,7 @@ interface Geolocation {
   accuracy?: number;
 }
 
-// const NYC_GEOLOC: Geolocation = { longitude: -73.935242, latitude: 40.73061 };
+const NYC_COORDS: Geolocation = { longitude: -73.935242, latitude: 40.73061 };
 
 export default function Home() {
   const [currentGeoLoc, setCurrentGeoLoc] = useState<
@@ -32,25 +32,19 @@ export default function Home() {
   return (
     <div className="h-full w-full p-20 bg-zinc-50 font-sans dark:bg-black">
       <div className="flex justify-center items-center h-[500px] w-full">
-        {currentGeoLoc ? (
-          <Map
-            initialViewState={{
-              longitude: currentGeoLoc.longitude,
-              latitude: currentGeoLoc.latitude,
-              zoom: 16,
-            }}
-            minZoom={10}
-            maxZoom={20}
-            mapStyle="https://api.maptiler.com/maps/streets/style.json?key=wj2xyR5KXFoBWjTtFjY7"
-          >
-            <Marker
-              longitude={currentGeoLoc.longitude}
-              latitude={currentGeoLoc.latitude}
-              anchor="center"
-            >
-              <div className="h-6 w-6 rounded-full border-4 border-red-500" />
-            </Marker>
-            {createMockCoords(currentGeoLoc, 0.002).map((e, i) => (
+        <Map
+          initialViewState={{
+            longitude: NYC_COORDS.longitude,
+            latitude: NYC_COORDS.latitude,
+            zoom: 16,
+          }}
+          minZoom={10}
+          maxZoom={20}
+          mapStyle="https://api.maptiler.com/maps/streets/style.json?key=wj2xyR5KXFoBWjTtFjY7"
+        >
+          {<FindingLocation loc={currentGeoLoc} />}
+          {currentGeoLoc &&
+            createMockCoords(currentGeoLoc, 0.002).map((e, i) => (
               <Marker
                 key={`mockLoc${i}`}
                 longitude={e.longitude}
@@ -60,21 +54,33 @@ export default function Home() {
                 <div className="h-4 w-4 rounded-full border-4 border-blue-800" />
               </Marker>
             ))}
-          </Map>
-        ) : (
-          <MapLoading text="Invasively finding your location..." />
-        )}
+        </Map>
       </div>
       {currentGeoLoc && <LocationDisplay geolocation={currentGeoLoc} />}
     </div>
   );
 }
 
-const MapLoading = ({ text }: { text: string }) => (
-  <div className="w-full h-full flex justify-center items-center border-2 border-dashed">
-    <div className="text-4xl text-center animate-pulse">{text}</div>
-  </div>
-);
+const FindingLocation = ({ loc }: { loc?: Geolocation }) => {
+  const { current: map } = useMap();
+
+  useEffect(() => {
+    if (map && loc) {
+      map.flyTo({
+        center: [loc.longitude, loc.latitude],
+        essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+      });
+    }
+  }, [map, loc]);
+
+  return loc ? (
+    <></>
+  ) : (
+    <div className="absolute left-1/2 top-1/2 -translate-1/2 rounded-full p-4 text-2xl bg-slate-900 text-slate-50 animate-pulse">
+      Getting Your Location
+    </div>
+  );
+};
 
 const LocationDisplay = ({ geolocation }: { geolocation: Geolocation }) => (
   <div className="w-full h-full flex justify-around py-4 text-2xl">
